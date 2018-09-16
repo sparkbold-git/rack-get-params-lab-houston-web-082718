216081a238aa681a444e2103de9263e5e66ded8b
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-16 00:30:27 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,38 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
+  @@items = ["Apples", "Carrots", "Pears"]
+  @@cart  = []
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    req  = Rack::Request.new(env)
 
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write('Your cart is empty')
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+
+    elsif req.path.match(/add/)
+      add_item = req.params["item"]
+      if @@items.include? add_item
+        @@cart << add_item
+        resp.write("added #{add_item}")
+      else
+        resp.write("We don't have that item!")
+      end
     else
       resp.write "Path Not Found"
     end
@@ -21,7 +41,7 @@ class Application
   end
 
   def handle_search(search_term)
-    if @@items.include?(search_term)
+    if @@items.include?(search_term.capitalize)
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
